6b1986c81a6eb56ae59e27e0ee51f63bb02ee84d
Yago Luiz <yago.luiz@hotmail.com>
2019-12-11 18:03:15 -0300

    Add middleware error and script in sql

---
+++ src/Discount.API/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Wiz.CRM.API.Middlewares
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ErrorHandlerMiddleware(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var ex = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+            if (ex == null)
+            {
+                return;
+            }
+
+            var problemDetails = new ProblemDetails
+            {
+                Title = "Internal Server Error",
+                Status = StatusCodes.Status500InternalServerError,
+                Instance = context.Request.Path.Value,
+                Detail = ex.Message
+            };
+
+            if (_webHostEnvironment.IsDevelopment())
+            {
+                problemDetails.Detail += $": {ex.StackTrace}";
+            }
+
+            context.Response.StatusCode = problemDetails.Status.Value;
+            context.Response.ContentType = "application/problem+json";
+
+            using var writer = new Utf8JsonWriter(context.Response.Body);
+            JsonSerializer.Serialize(writer, problemDetails);
+            await writer.FlushAsync();
+        }
+    }
+}
